29b90b639dc01748df912528fdf4798ce935e707
tanviranik004 <tanvir.anik.009@gmail.com>
2020-08-11 23:27:17 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,39 @@
+const age= 4;
+if(age>0){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+
+}
+
+const name= '';
+if(name.length>0){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+    
+}
+
+
+
+ let Name=NaN;
+console.log(Name);
+ if(Name){
+    console.log('condition is true');
+ }
+else{
+    console.log('condition is false');
+    
+}
+
+let aName= 12;
+
+ if(aName || aName==0){
+    console.log('condition is true');
+ }
+else{
+    console.log('condition is false');
+    
+}
