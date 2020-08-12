df4a6e0d8cc1c8b38d56524506a0093544a005d9
tanviranik004 <tanvir.anik.009@gmail.com>
2020-08-12 19:52:10 +0600

    map-filter

---
+++ map-filter.js
@@ -0,0 +1,42 @@
+//const numbers = [3,4,5,6,7,8];
+// const output = [];
+// for(let i = 0; i<numbers.length; i++){
+//     const element = numbers[i];
+//     const result = element * element;
+//     output.push(result);
+// }
+//console.log(output);
+// function square(element){
+//     return element*element;
+
+// }
+// numbers.map(function(element,index,array){
+//     console.log(element, index,array);
+//     return element*element;
+// })
+
+// const numbers = [3,4,5,6,7,8];
+
+// function square(element){
+//     return element*element;
+
+// }
+
+// const result= numbers.map(function(element,index,array){
+//     //console.log(element, index,array);
+//     return element*element;
+// })
+// console.log(result);
+
+// const numbers = [3,4,5,6,7,8];
+// const result = numbers.map(x => x * x);
+// console.log(result);
+
+// const numbers = [3,4,5,6,7,8];
+// const bigger = numbers.filter( x => x>5);
+// console.log(bigger)
+const numbers = [3,4,5,6,7,8];
+const isThere = numbers.find( x => x>5);
+
+console.log(isThere);
+
