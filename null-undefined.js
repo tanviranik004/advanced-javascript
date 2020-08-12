fa924e9bad59eb42df85853493068b422325c537
tanviranik004 <tanvir.anik.009@gmail.com>
2020-08-12 07:15:54 +0600

    null-undefined

---
+++ null-undefined.js
@@ -0,0 +1,20 @@
+//let pakhi;
+ function add(num1,num2){
+     console.log(num1+ num2);
+     return
+}
+
+
+function add(num1,num2=10){
+     console.log(num1,num2);
+ }
+ const result = add(13);
+console.log( result)
+ const premik = {name: 'smart dude', phone:654567};
+ console.log(premik.gf)
+
+let fun = undefined;
+console.log(fun)
+
+let ages =[2, 5, 8]
+console.log(ages[11]);
