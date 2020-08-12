e75a77dc54237989c58858b12683eb865814c5d3
tanviranik004 <tanvir.anik.009@gmail.com>
2020-08-12 08:15:01 +0600

    double-treple-equal

---
+++ double-treple-equal.js
@@ -0,0 +1,78 @@
+//1
+const first = 0;
+const second = false;
+if( first === second){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+
+//2
+const first1 = 0;
+const second1 = false;
+if( first1 == second1){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+
+//3
+const first2 = 2;
+const second2 = 3;
+if( first2 == second2){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+//4
+const first3 = 2;
+const second3 = '2';
+if( first3 == second3){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+//5
+const first4 = 2;
+const second4 = '2';
+if( first4 === second4){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+
+//6
+const first5 = 2;
+const second5 = true;
+if( first5 == second5){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+
+//7
+const first6 = 1;
+const second6 = true;
+if( first6 == second6){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+
+//=== value and type duitai check korbe
+//== type ta ke convert kore dekbe je kasa kasi ki vaue ase  tarpor setake eki type e convert kore compare kore dekbe je tik ase ki na
+
+
+
+// if (‘2’===2)
+// {console.log(“Inside if”);} 
+
+// else
+// { console.log(“Inside else”);}
